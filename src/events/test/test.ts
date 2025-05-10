6d2962a1492648a872d119d26fd865cc69fd4626
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-05-10 11:42:31 -0500

    chore: little fix

--- src/events/test/test.ts
+++ src/events/test/test.ts
@@ -1,11 +1,11 @@
 import { Events, type Message } from 'discord.js';
-import type { EventHandler } from '../core/eventHandler';
+import type { EventHandler } from '../../core/eventHandler';
 
 const messageCreate: EventHandler<typeof Events.MessageCreate> = {
 	name: Events.MessageCreate,
 	execute(message: Message) {
 		console.log(message)
-		if (message.content === '!prueba') {
+		if (message.content === '!test') {
 			message.reply('Hola soy HeapBot! Mi nombre es alucivo a la Heap Memory');
 		}
 	},
