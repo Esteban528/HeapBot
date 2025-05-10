929ea27b235336a65cf9861d43d37a72aba163e0
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-05-10 11:47:06 -0500

    chore: add test event

--- src/events/test/test.ts
+++ src/events/test/test.ts
@@ -4,8 +4,7 @@ import type { EventHandler } from '../../core/eventHandler';
 const messageCreate: EventHandler<typeof Events.MessageCreate> = {
 	name: Events.MessageCreate,
 	execute(message: Message) {
-		console.log(message)
-		if (message.content === '!test') {
+		if (message.content === '!heapbot') {
 			message.reply('Hola soy HeapBot! Mi nombre es alucivo a la Heap Memory');
 		}
 	},
